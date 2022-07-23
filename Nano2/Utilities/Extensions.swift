3a50f9986b5e23f262a0a994475328915c0c11f4
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-07-23 14:47:28 +0700

    - Remove unnecessary functions

--- Nano2/Utilities/Extensions.swift
+++ Nano2/Utilities/Extensions.swift
@@ -26,12 +26,12 @@ extension UIImage {
             width: size.width * scaleFactor,
             height: size.height * scaleFactor
         )
-
+        
         // Draw and return the resized UIImage
         let renderer = UIGraphicsImageRenderer(
             size: scaledImageSize
         )
-
+        
         let scaledImage = renderer.image { _ in
             self.draw(in: CGRect(
                 origin: .zero,
@@ -51,70 +51,83 @@ extension UIColor {
 
 extension UIView {
     
-    func anchor(top: NSLayoutYAxisAnchor? = nil,
-                   left: NSLayoutXAxisAnchor? = nil,
-                   bottom: NSLayoutYAxisAnchor? = nil,
-                   right: NSLayoutXAxisAnchor? = nil,
-                   paddingTop: CGFloat = 0,
-                   paddingLeft: CGFloat = 0,
-                   paddingBottom: CGFloat = 0,
-                   paddingRight: CGFloat = 0,
-                   width: CGFloat? = nil,
-                   height: CGFloat? = nil) {
-           
-           translatesAutoresizingMaskIntoConstraints = false
-           
-           if let top = top {
-               topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
-           }
-           
-           if let left = left {
-               leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
-           }
-           
-           if let bottom = bottom {
-               bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
-           }
-           
-           if let right = right {
-               rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
-           }
-           
-           if let width = width {
-               widthAnchor.constraint(equalToConstant: width).isActive = true
-           }
-           
-           if let height = height {
-               heightAnchor.constraint(equalToConstant: height).isActive = true
-           }
-       }
-       
-       func centerX(inView view: UIView) {
-           translatesAutoresizingMaskIntoConstraints = false
-           centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-       }
-       
-       func centerY(inView view: UIView, leftAnchor: NSLayoutXAxisAnchor? = nil,
-                    paddingLeft: CGFloat = 0, constant: CGFloat = 0) {
-           
-           translatesAutoresizingMaskIntoConstraints = false
-           centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).isActive = true
-           
-           if let left = leftAnchor {
-               anchor(left: left, paddingLeft: paddingLeft)
-           }
-       }
-       
-       func setDimensions(height: CGFloat, width: CGFloat) {
-           translatesAutoresizingMaskIntoConstraints = false
-           heightAnchor.constraint(equalToConstant: height).isActive = true
-           widthAnchor.constraint(equalToConstant: width).isActive = true
-       }
+    func anchor(
+        top: NSLayoutYAxisAnchor? = nil,
+        left: NSLayoutXAxisAnchor? = nil,
+        bottom: NSLayoutYAxisAnchor? = nil,
+        right: NSLayoutXAxisAnchor? = nil,
+        paddingTop: CGFloat = 0,
+        paddingLeft: CGFloat = 0,
+        paddingBottom: CGFloat = 0,
+        paddingRight: CGFloat = 0,
+        width: CGFloat? = nil,
+        height: CGFloat? = nil) {
+            
+            translatesAutoresizingMaskIntoConstraints = false
+            
+            if let top = top {
+                topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+            }
+            
+            if let left = left {
+                leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+            }
+            
+            if let bottom = bottom {
+                bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+            }
+            
+            if let right = right {
+                rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+            }
+            
+            if let width = width {
+                widthAnchor.constraint(equalToConstant: width).isActive = true
+            }
+            
+            if let height = height {
+                heightAnchor.constraint(equalToConstant: height).isActive = true
+            }
+        }
     
-    @discardableResult
-    open func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) -> AnchoredConstraints {
+    func centerX(inView view: UIView) {
         
         translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func centerY(
+        inView view: UIView,
+        leftAnchor: NSLayoutXAxisAnchor? = nil,
+        paddingLeft: CGFloat = 0,
+        constant: CGFloat = 0) {
+            
+            translatesAutoresizingMaskIntoConstraints = false
+            centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).isActive = true
+            
+            if let left = leftAnchor {
+                anchor(left: left, paddingLeft: paddingLeft)
+            }
+        }
+    
+    func setDimensions(height: CGFloat, width: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    
+    @discardableResult
+    open func anchor(
+        top: NSLayoutYAxisAnchor?,
+        leading: NSLayoutXAxisAnchor?,
+        bottom: NSLayoutYAxisAnchor?,
+        trailing: NSLayoutXAxisAnchor?,
+        padding: UIEdgeInsets = .zero,
+        size: CGSize = .zero) -> AnchoredConstraints {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+            
         var anchoredConstraints = AnchoredConstraints()
         
         if let top = top {
@@ -148,13 +161,16 @@ extension UIView {
     
     @discardableResult
     open func fillSuperview(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+        
         translatesAutoresizingMaskIntoConstraints = false
+        
         let anchoredConstraints = AnchoredConstraints()
+        
         guard let superviewTopAnchor = superview?.topAnchor,
-            let superviewBottomAnchor = superview?.bottomAnchor,
-            let superviewLeadingAnchor = superview?.leadingAnchor,
-            let superviewTrailingAnchor = superview?.trailingAnchor else {
-                return anchoredConstraints
+              let superviewBottomAnchor = superview?.bottomAnchor,
+              let superviewLeadingAnchor = superview?.leadingAnchor,
+              let superviewTrailingAnchor = superview?.trailingAnchor else {
+            return anchoredConstraints
         }
         
         return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
@@ -162,13 +178,15 @@ extension UIView {
     
     @discardableResult
     open func fillSuperviewSafeAreaLayoutGuide(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+        
         let anchoredConstraints = AnchoredConstraints()
+        
         if #available(iOS 11.0, *) {
             guard let superviewTopAnchor = superview?.safeAreaLayoutGuide.topAnchor,
-                let superviewBottomAnchor = superview?.safeAreaLayoutGuide.bottomAnchor,
-                let superviewLeadingAnchor = superview?.safeAreaLayoutGuide.leadingAnchor,
-                let superviewTrailingAnchor = superview?.safeAreaLayoutGuide.trailingAnchor else {
-                    return anchoredConstraints
+                  let superviewBottomAnchor = superview?.safeAreaLayoutGuide.bottomAnchor,
+                  let superviewLeadingAnchor = superview?.safeAreaLayoutGuide.leadingAnchor,
+                  let superviewTrailingAnchor = superview?.safeAreaLayoutGuide.trailingAnchor else {
+                return anchoredConstraints
             }
             return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
             
@@ -178,7 +196,9 @@ extension UIView {
     }
     
     open func centerInSuperview(size: CGSize = .zero) {
+        
         translatesAutoresizingMaskIntoConstraints = false
+        
         if let superviewCenterXAnchor = superview?.centerXAnchor {
             centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
         }
@@ -197,14 +217,18 @@ extension UIView {
     }
     
     open func centerXToSuperview() {
+        
         translatesAutoresizingMaskIntoConstraints = false
+        
         if let superviewCenterXAnchor = superview?.centerXAnchor {
             centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
         }
     }
     
     open func centerYToSuperview() {
+        
         translatesAutoresizingMaskIntoConstraints = false
+        
         if let superviewCenterYAnchor = superview?.centerYAnchor {
             centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
         }
@@ -212,30 +236,42 @@ extension UIView {
     
     @discardableResult
     open func constrainHeight(_ constant: CGFloat) -> AnchoredConstraints {
+        
         translatesAutoresizingMaskIntoConstraints = false
+        
         var anchoredConstraints = AnchoredConstraints()
         anchoredConstraints.height = heightAnchor.constraint(equalToConstant: constant)
         anchoredConstraints.height?.isActive = true
+        
         return anchoredConstraints
     }
     
     @discardableResult
     open func constrainWidth(_ constant: CGFloat) -> AnchoredConstraints {
+        
         translatesAutoresizingMaskIntoConstraints = false
+        
         var anchoredConstraints = AnchoredConstraints()
         anchoredConstraints.width = widthAnchor.constraint(equalToConstant: constant)
         anchoredConstraints.width?.isActive = true
+        
         return anchoredConstraints
     }
     
-    open func setupShadow(opacity: Float = 0, radius: CGFloat = 0, offset: CGSize = .zero, color: UIColor = .black) {
-        layer.shadowOpacity = opacity
-        layer.shadowRadius = radius
-        layer.shadowOffset = offset
-        layer.shadowColor = color.cgColor
+    open func setupShadow(
+        opacity: Float = 0,
+        radius: CGFloat = 0,
+        offset: CGSize = .zero,
+        color: UIColor = .black) {
+        
+            layer.shadowOpacity = opacity
+            layer.shadowRadius = radius
+            layer.shadowOffset = offset
+            layer.shadowColor = color.cgColor
     }
     
     convenience public init(backgroundColor: UIColor = .clear) {
+        
         self.init(frame: .zero)
         self.backgroundColor = backgroundColor
     }
@@ -245,49 +281,53 @@ extension UIFont {
     
     static func largeTitle() -> UIFont {
         
-      guard let customFont = UIFont(name: "Raleway-Bold", size: 36) else {
-        return UIFont.systemFont(ofSize: 36)
-      }
+        guard let customFont = UIFont(name: "Raleway-Bold", size: 36) else {
+            return UIFont.systemFont(ofSize: 36)
+        }
         
-      return customFont
+        return customFont
     }
     
     static func heading() -> UIFont {
         
-      guard let customFont = UIFont(name: "Raleway-Bold", size: 32) else {
-        return UIFont.systemFont(ofSize: 32)
-      }
+        guard let customFont = UIFont(name: "Raleway-Bold", size: 32) else {
+            return UIFont.systemFont(ofSize: 32)
+        }
         
-      return customFont
+        return customFont
     }
     
     static func subHeading() -> UIFont {
         
-      guard let customFont = UIFont(name: "Raleway-SemiBold", size: 20) else {
-        return UIFont.systemFont(ofSize: 20)
-      }
+        guard let customFont = UIFont(name: "Raleway-SemiBold", size: 20) else {
+            return UIFont.systemFont(ofSize: 20)
+        }
         
-      return customFont
+        return customFont
     }
     
     static func body() -> UIFont {
         
-      guard let customFont = UIFont(name: "Raleway-Medium", size: 16) else {
-        return UIFont.systemFont(ofSize: 16)
-      }
+        guard let customFont = UIFont(name: "Raleway-Medium", size: 16) else {
+            return UIFont.systemFont(ofSize: 16)
+        }
         
-      return customFont
+        return customFont
     }
 }
 
 extension UITextField {
-
+    
     func addDoneButton(title: String, target: Any, selector: Selector) {
-
-        let toolBar = UIToolbar(frame: CGRect(x: 0.0,
-                                              y: 0.0,
-                                              width: UIScreen.main.bounds.size.width,
-                                              height: 44.0))
+        
+        let toolBar = UIToolbar(
+            frame: CGRect(
+                x: 0.0,
+                y: 0.0,
+                width: UIScreen.main.bounds.size.width,
+                height: 44.0)
+        )
+        
         let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let barButton = UIBarButtonItem(title: title, style: .plain, target: target, action: selector)//3
         toolBar.setItems([flexible, barButton], animated: false)
--- Nano2/Utilities/Utilities.swift
+++ Nano2/Utilities/Utilities.swift
@@ -9,38 +9,6 @@ import UIKit
 
 class Utilities {
     
-    func inputContainerView(withImage image: UIImage, textField: UITextField) -> UIView {
-        
-        let view = UIView()
-        let iv = UIImageView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 14
-        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        iv.image = image
-        view.addSubview(iv)
-        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 12, paddingBottom: 12)
-        iv.contentMode = .scaleAspectFit
-        iv.setDimensions(height: 24, width: 24)
-        
-        view.addSubview(textField)
-        textField.anchor(left: iv.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 12, paddingBottom: 12)
-        
-        return view
-    }
-    
-    func attributedButton(_ firstPart: String, _ secondPart: String) -> UIButton {
-        let button = UIButton(type: .system)
-        
-        let attributedTitle = NSMutableAttributedString(string: firstPart, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.white])
-        
-        attributedTitle.append(NSAttributedString(string: secondPart, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.white]))
-        
-        button.setAttributedTitle(attributedTitle, for: .normal)
-        
-        return button
-    }
-    
     func vibrate(for type: UINotificationFeedbackGenerator.FeedbackType) {
         
         DispatchQueue.main.async {
